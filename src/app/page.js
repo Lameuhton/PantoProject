2c78d1d0bc4b5032e51a8d4b2420d0ad17f386ce
Lameuhton <aurelianeg2000@gmail.com>
2024-08-23 14:27:21 +0200

    finished section 5

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 import Section1 from "@/components/Section1";
 import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
+import Section4 from "@/components/Section4";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Section1 />
         <Section2 />
         <Section3 />
+        <Section4 />
       </main>
       <Footer />
     </>
